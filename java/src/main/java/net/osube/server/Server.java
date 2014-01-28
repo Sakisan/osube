3a4285980cef0bc13e0880ebae9a891e1e2fd935
Sakisan <sakileon101@gmail.com>
2014-01-28 23:30:27 +0100

    java module

---
+++ java/src/main/java/net/osube/server/Server.java
@@ -0,0 +1,12 @@
+package net.osube.server;
+
+/**
+ * Created by: Antoine Snyers (antoine at atmire dot com)
+ * Date: 28 Jan 2014
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
